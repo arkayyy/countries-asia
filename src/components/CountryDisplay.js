e787febcce59ba0f66a5a1baabb5b07fa58c0366
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-09-14 15:30:37 +0530

    push

--- src/components/CountryDisplay.js
+++ src/components/CountryDisplay.js
@@ -4,12 +4,10 @@ import {getCountriesAsia} from '../redux/index';
 import '../styles/CountryDisplay.css';
 
 const CountryDisplay=({countriesAsia, fetchCountryStates})=> {
+    var ctr = 0;
     return (
         <div>
             <h1>COUNTRIES IN ASIA</h1>
-
-    
-
 
             <button onClick={()=>{fetchCountryStates();}}>FETCH COUNTRIES</button>
             
@@ -26,8 +24,16 @@ const CountryDisplay=({countriesAsia, fetchCountryStates})=> {
                         <h2>REGION: {country.region}</h2>
                         <h2>SUB-REGION: {country.subregion}</h2>
                         <h2>POPULATION: {country.population}</h2>
-                        <div><h2 style={{display:"inline"}}>BORDERS:</h2> <p style={{fontSize:"25px"}}><ul style={{listStyle:"none",display:"flex" , flexWrap:"wrap", justifyContent:"center"}}>{country.borders.map((border)=>(<li style={{display:"inline", margin:"4px"}}>{border}</li>))}</ul></p></div>
-                        <div><h2>LANGUAGES:</h2> <p style={{fontSize:"25px"}}><ul style={{listStyle:"none"}}>{country.languages.map((lang)=>(<li style={{display:"inline", margin:"4px"}}>{lang.name}</li>))}</ul></p></div>
+                        
+                        <div>
+                            <h2 style={{display:"inline"}}>BORDERS:</h2> 
+                            <div style={{fontSize:"25px"}}><ul style={{listStyle:"none",display:"flex" , flexWrap:"wrap", justifyContent:"center"}}>{country.borders.map((border)=>(<li key={border+toString(ctr++)} style={{display:"inline", margin:"4px"}}>{border}</li>))}</ul></div>
+                            
+                        </div>
+
+
+                        <div><h2>LANGUAGES:</h2> 
+                        <div style={{fontSize:"25px"}}><ul style={{listStyle:"none"}}>{country.languages.map((lang)=>(<li key={lang.name+toString(ctr++)} style={{display:"inline", margin:"4px"}}>{lang.name}</li>))}</ul></div></div>
                         </div>
 
                         <div className="flag">
